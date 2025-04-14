6f6ed4a258c0e3b11f0617e9ed1e35e4a7e5ded4
sanjanaynvsdl <sanjanayalamarthi@gmail.com>
2025-04-14 11:03:58 +0530

    BE done

--- backend/index.js
+++ backend/index.js
@@ -32,8 +32,10 @@ app.use("/api/v1/admin", adminRoutes);
 app.use("/api/v1/rider", riderRoutes);
 
 
-app.listen(3000,()=>{
-    console.log("Server is listening to port 3000")
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT,()=>{
+    console.log(`Server is listening to port ${PORT}`)
 });
 
 
