f2e1a8afcceaab4fff0c4fc85219af1f60a2f9d8
sanjanaynvsdl <sanjanayalamarthi@gmail.com>
2025-04-14 21:41:02 +0530

    fixed backned url issues

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
 
 // Create an axios instance with default config
+const backendUrl =  import.meta.env.VITE_API_URL
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3000/api/v1',
+  baseURL: `${backendUrl}/api/v1`,
   headers: {
     'Content-Type': 'application/json',
   },
